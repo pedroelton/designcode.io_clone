8de17c5a45013ad3f477e4d0ea32c6c31335db5e
Pedro Oliveira <pedrooliveira@Pedros-MacBook-Pro.local>
2022-04-28 12:27:03 +0900

    fixed state at the tooltip

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,6 @@
 // Importing all libs and files used
 import { Link } from "gatsby"
-import React, { useState } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import styled from "styled-components"
 import { menuData } from "../../data/MenuData"
 import MenuButton from "../buttons/MenuButton"
@@ -10,17 +10,40 @@ import MenuTooltip from "../tooltips/MenuTooltip"
 export default function Header() {
   //declaring the isOpen useState hook to a boolean.
   const [isOpen, setIsOpen] = useState(false)
+  // reference to the component where the state will work
+  const ref = useRef()
+  const tooltipRef = useRef()
+
   function handleClick(event) {
     setIsOpen(!isOpen)
     event.preventDefault()
   }
+
+  function handleClickOutside(event) {
+    // if the component refered is opened and contains event. Then close it when clicked outside
+    if (
+      ref.current &&
+      !ref.current.contains(event.target) &&
+      !tooltipRef.current.contains(event.target)
+    ) {
+      setIsOpen(false)
+    }
+  }
+  // close the tooltip when clicked outside
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside)
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [])
 
   return (
     <Wrapper onClick={() => setIsOpen(!isOpen)}>
       <Link to="/">
         <img src="/images/logos/logo.svg" alt="Logo" />
       </Link>
-      <MenuWrapper count={menuData.length}>
+      <MenuWrapper count={menuData.length} ref={ref}>
         {/* looping the menuData data inside the MenuButton component 
               the onClick event toggles the handleClick function which receives
               setIsOpen(!isOpen) and the prevent default
@@ -39,12 +62,15 @@ export default function Header() {
         )}
         <HamburgerWrapper>
           <MenuButton
-            item={{ title: "", icon: "/images/icons/hamburger.svg", link: "" }}
+            item={{ title: "", icon: "/images/icons/hamburger.svg", link: "/" }}
+            onClick={event => handleClick(event)}
           />
         </HamburgerWrapper>
       </MenuWrapper>
       {/* This is the tooltip, and it receives the prop isOpen to toggle visibility */}
-      <MenuTooltip isOpen={isOpen} />
+      <div ref={tooltipRef}>
+        <MenuTooltip isOpen={isOpen} />
+      </div>
     </Wrapper>
   )
 }
